95a7c704948a262c4f67ad77e3f2aa3a0fba4892
Evelyn Sjafii <evelynsj98@gmail.com>
2020-05-16 15:17:32 -0700

    finish black jumps and possible moves functions

--- oskaplayer.py
+++ oskaplayer.py
@@ -12,8 +12,8 @@ MOVE_DOWN = "move down"
 MOVE_DOWN_LEFT = "move down left"
 MOVE_DOWN_RIGHT = "move down right"
 JUMP_DOWN = "jump down"
-JUMP_DIAG_LEFT = "jump diagonal left"
-JUMP_DIAG_RIGHT = "jump diagonal right"
+JUMP_DOWN_LEFT = "jump down left"
+JUMP_DOWN_RIGHT = "jump down right"
 JUMP_DOWN_DIAG_RIGHT = "jump down diagonal right"
 JUMP_DIAG_LEFT_DOWN = "jump diagonal left down"
 
@@ -21,6 +21,11 @@ JUMP_DIAG_LEFT_DOWN = "jump diagonal left down"
 MOVE_UP = "move up"
 MOVE_UP_LEFT = "move up left"
 MOVE_UP_RIGHT = "move up right"
+JUMP_UP = "jump up"
+JUMP_UP_LEFT = "jump up left"
+JUMP_UP_RIGHT = "jump up right"
+JUMP_UP_DIAG_RIGHT = "jump up diagonal right"
+JUMP_DIAG_LEFT_UP = "jump diagonal left up"
 
 
 class Game:
@@ -44,6 +49,14 @@ class Game:
                     # TODO: make moves, return list of boards
 
     def possibleMoves(self, i, j):
+        '''
+            This function determines possible moves and/or jumps that player can make
+            Parameters:
+                @i: row position of player
+                @j: column position of player
+            Return:
+                @moves: list of moves and/or jumps returned by helper functions
+        '''
         moves = []
         if (self.player == WHITE):
             whiteMoves = self.possibleWhiteMoves(i, j)
@@ -54,10 +67,14 @@ class Game:
                 moves = moves + whiteJumps
         elif (self.player == BLACK):
             blackMoves = self.possibleBlackMoves(i, j)
+            blackJumps = self.possibleBlackJumps(i, j)
             if (blackMoves):
                 moves = moves + blackMoves
-            # TODO: blackjumps
+            if (blackJumps):
+                moves = moves + blackJumps
 
+        return moves
+            
     def possibleWhiteMoves(self, i, j):
         '''
             This function returns all possible moves that white player can make
@@ -107,14 +124,22 @@ class Game:
                     moves.append(JUMP_DOWN)
                 # row before midpoint - 1
                 if (i < self.state.midpoint - 1 and j - 2 >= 0 and board[i + 1][j - 1] == BLACK and board[i + 2][j - 2] == EMPTY_SPACE):
-                    moves.append(JUMP_DIAG_LEFT)
+                    moves.append(JUMP_DOWN_LEFT)
                 # row at and after midpoint
                 elif (j + 2 < len(board[i + 2]) and board[i + 1][j + 1] == BLACK and board[i + 2][j + 2] == EMPTY_SPACE):
-                    moves.append(JUMP_DIAG_RIGHT)
+                    moves.append(JUMP_DOWN_RIGHT)
 
         return moves
 
     def possibleBlackMoves(self, i, j):
+        '''
+            This function returns all possible moves that black player can make
+            Parameters:
+                @i: row position of black player
+                @j: column position of black player
+            Return:
+                @moves: list of all possible moves i.e. move up, move up left, move up right by verifying the bounds and whether the space is occupied or not. The move names do not reflect the actual oska board but rather the internal representation.
+        '''
         moves = []
         board = self.state.board
 
@@ -131,6 +156,36 @@ class Game:
                     moves.append(MOVE_UP_LEFT)
         return moves
 
+    def possibleBlackJumps(self, i, j):
+        '''
+            This function returns all possible jumps that black player can make
+            Parameters:
+                @i: row position of black player
+                @j: column position of black player
+            Return:
+                @moves: list of all possible jumps by verifying the bounds, if the space is occupied, and that it skips over white player. The move names do not reflect the actual oska board but rather the internal representation.
+        '''
+        moves = []
+        board = self.state.board
+
+        if (i - 2 >= 0):
+            if (i == self.state.midpoint + 1):  # row after midpoint
+                if (j + 1 < len(board[i - 2]) and board[i - 1][j] == WHITE and board[i - 2][j + 1] == EMPTY_SPACE):
+                    moves.append(JUMP_UP_DIAG_RIGHT)
+                if (j - 1 >= 0 and board[i - 1][j - 1] == WHITE and board[i - 2][j - 1] == EMPTY_SPACE):
+                    moves.append(JUMP_DIAG_LEFT_UP)
+            else:
+                # black can jump up at row after midpoint + 1 and at and before midpoint
+                if (j < len(board[i - 2]) and board[i - 1][j] == WHITE and board[i - 2][j] == EMPTY_SPACE):
+                    moves.append(JUMP_UP)
+                # row after midpoint + 1
+                if (i > self.state.midpoint + 1 and j - 2 >= 0 and board[i - 1][j - 1] == WHITE and board[i - 2][j - 2] == EMPTY_SPACE):
+                    moves.append(JUMP_UP_LEFT)
+                # row at and before midpoint
+                elif (i <= self.state.midpoint and j + 2 < len(board[i - 2]) and board[i - 1][j + 1] == WHITE and board[i - 2][j + 2] == EMPTY_SPACE):
+                    moves.append(JUMP_UP_RIGHT)
+
+        return moves
 
 class Board:
     def __init__(self, input):
